01c87e26ca34eefc5a0d0781ac842f1f807b8a1b
George Cooper <coopsgeorge@gmail.com>
2020-03-05 18:58:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,15 +13,11 @@ def directors_totals(nds)
     director_name = nds[director_index][:name]
     movie_index = 0
     result[director_name] =  0
-  #inner_len = nds[director_index][:movies].length
+    
   while movie_index < nds[director_index][:movies].length do
       individual_gross = 0
 
       result[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
-#binding.pry
-      #individual_gross += num
-
-      #result[director_name] = individual_gross
 
       movie_index += 1
     end
